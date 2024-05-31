36d475642c330b1a27a8376045d0e79a4df66be1
Shishir <shishirk2801@gmail.com>
2024-05-31 13:40:03 +0530

    Change the boilerplate

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
+
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import './demos/ipc'
+// If you want use Node.js, the`nodeIntegration` needs to be enabled in the Main process.
+// import './demos/node'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+postMessage({ payload: 'removeLoading' }, '*')
