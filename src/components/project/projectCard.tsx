846c6dd36444cb1ed945c3875effbe6746d28ffe
Shishir <shishirk2801@gmail.com>
2024-06-03 03:22:02 +0530

    Add ability to store time spent to db

--- src/components/project/projectCard.tsx
+++ src/components/project/projectCard.tsx
@@ -2,14 +2,16 @@ import { useState, useRef } from "react";
 import { Link } from "react-router-dom";
 
 const ProjectCard = (props) => {
-  const { projectName, projectPath } = props;
+  const { project } = props;
   const [isTimerRunning, setIsTimerRunning] = useState(false);
   const [elapsedTime, setElapsedTime] = useState(0);
+  const [startTimestamp, setStartTimestamp] = useState(0);
   const intervalRef = useRef(null);
 
   const startTimer = () => {
     if (!isTimerRunning) {
       setIsTimerRunning(true);
+      setStartTimestamp(Date.now());
       intervalRef.current = setInterval(() => {
         setElapsedTime((prevElapsedTime) => prevElapsedTime + 1);
       }, 1000);
@@ -20,8 +22,20 @@ const ProjectCard = (props) => {
     if (isTimerRunning) {
       clearInterval(intervalRef.current);
       setIsTimerRunning(false);
+      const elapsedSeconds = Math.round((Date.now() - startTimestamp) / 1000);
+      props.updateUsage(elapsedSeconds, project.folderPath);
       setElapsedTime(0);
     }
+  };
+
+  const calculateTotalHours = () => {
+    const today = new Date().toISOString().split("T")[0];
+    const project = props.project;
+    if (!project || !project.usage[today]) return "0";
+    const totalSeconds = project.usage[today];
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    return `${hours} hours ${minutes} minutes`;
   };
 
   return (
@@ -30,7 +44,7 @@ const ProjectCard = (props) => {
         href="#"
         className="inline-flex m-2 text-4xl font-medium items-center hover:underline dark:text-white"
       >
-        {projectName}
+        {project.project}
       </a>
       {isTimerRunning && (
         <p className="mx-2 text-font-semibold tracking-tight text-gray-900 dark:text-white">
@@ -62,17 +76,25 @@ const ProjectCard = (props) => {
           {" "}
           <button
             className=" m-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-            onClick={() => window.ipcRenderer.openProjectDir(projectPath)}
+            onClick={() =>
+              window.ipcRenderer.openProjectDir(project.folderPath)
+            }
           >
             <i className="fa-solid fa-folder-open"></i>
           </button>
         </div>
-        <Link
-          to="/project"
-          className="m-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          <i className="fa-solid fa-arrow-right"></i>
-        </Link>
+        <div>
+          <Link
+            to="/project"
+            className="m-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            <i className="fa-solid fa-arrow-right m-2 p-2"></i>
+          </Link>
+        </div>
+
+        <div className="text-center">
+          Total Hours Today: {calculateTotalHours()}
+        </div>
       </div>
     </div>
   );
--- src/components/project/projects.tsx
+++ src/components/project/projects.tsx
@@ -4,6 +4,24 @@ import ProjectCard from "./projectCard";
 const Projects = (props) => {
   const { projects } = props;
 
+  const updateUsage = (elapsedSeconds, projectPath) => {
+    const today = new Date().toISOString().split("T")[0];
+    const updatedProjects = projects.map((project) => {
+      if (project.folderPath === projectPath) {
+        const usage = project.usage[today] || 0;
+        return {
+          ...project,
+          usage: {
+            ...project.usage,
+            [today]: usage + elapsedSeconds,
+          },
+        };
+      }
+      return project;
+    });
+    // Update the projects in your store
+    window.ipcRenderer.setProjectData(updatedProjects);
+  };
   return (
     <div className="w-full p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
       <div className="mt-4">
@@ -16,8 +34,8 @@ const Projects = (props) => {
           return (
             <ProjectCard
               key={index}
-              projectName={project.project}
-              projectPath={project.folderPath}
+              project={project}
+              updateUsage={updateUsage}
             />
           );
         })}
